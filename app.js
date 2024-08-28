947ad2436f7e9917e046fa942063cfa255c65805
iCeeqSolutions <155289437+iceeqsolutions@users.noreply.github.com>
2024-08-28 12:30:35 +0300

    ADD initial project structure with files & folders
    - Add "type" = "module" to package.json in order to use ESM syntax
    - Install EJS and define the view engine in the app.file
    - Add views -folder for improved project structure and management
    - Add initial views into the views -folder and add boilerplate
    - Add express app in app.js, define routes to views and test the links

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("Hello world");
+import express from "express";
+
+const app = express();
+
+// Specify which view engine to use
+app.set("view engine", "ejs");
+
+app.listen(3000);
+
+// Render views
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/blogs/create", (req, res) => {
+    res.render("create");
+});
+
+// Test redirect
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404
+app.use((req, res) => {
+    res.status(404).render("404");
+});
